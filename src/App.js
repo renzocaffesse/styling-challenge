5eacc9a98a4b6555297ec9955c8d2d954ab8ec43
Renzo Caffesse <renzo.caffesse@gmail.com>
2025-02-25 17:28:36 -0300

    feat: add basic structure for FirmFactCard and Button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./styles/GlobalStyles.scss";
+import FirmFacts from "./components/FirmFacts";
 
 function App() {
-  return <div className="App">Styling Challenge</div>;
+  return (
+    <div className="App">
+      <FirmFacts />
+    </div>
+  );
 }
 
 export default App;
