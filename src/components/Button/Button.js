780a0628c5c1ffe65db4374b70f727772e82dd20
Renzo Caffesse <renzo.caffesse@gmail.com>
2025-02-28 16:06:06 -0300

    feat: finalize styling and responsiveness for buttons and cards

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,23 @@
 import React from "react";
+import styles from "./Button.module.scss";
+import { ReactComponent as SearchIcon } from "../../assets/icons/Shape.svg";
 
-const Button = ({ children }) => {
-  return <button>{children}</button>;
+const Button = ({ variant = "default", disabled = false, children }) => {
+  return (
+    <button
+      className={`${styles.button} ${variant === "icon" ? styles.icon : ""} ${
+        disabled ? styles.disabled : ""
+      }`}
+      disabled={disabled}
+    >
+      {variant === "icon" && (
+        <span className={styles.icon}>
+          <SearchIcon className={styles.iconSvg} />
+        </span>
+      )}
+      <span className={styles.text}>{children}</span>
+    </button>
+  );
 };
 
 export default Button;
